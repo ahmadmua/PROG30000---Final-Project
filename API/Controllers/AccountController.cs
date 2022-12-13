c6e4f6953d8b8d827593e0bc59148bf0d0b3e09f
Muaz Ahmad <ahmadmua@sheridancollege.ca>
2022-12-12 19:41:37 -0500

    account authorization setup

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.models;
+using API.models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+
+        private TokenService _tokenService;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, 
+        TokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("signUp")]
+        public async Task<IActionResult> SignUpAsync(RegisterDto registerDto){
+
+            var user = new AppUser{
+            
+                Email = registerDto.Email,
+                UserName = registerDto.UserName,
+                Review = ""
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if(result.Succeeded){
+                return Ok();
+            }
+
+            return BadRequest(result.Errors);
+            
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginDto loginDto){
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if(user == null){
+                return NotFound();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(result.Succeeded){
+                return Ok(_tokenService.CreateToken(user));
+            }
+
+            return Unauthorized();
+        }
+    }
+}
