ba94c807a92af2d8c048afc8219e8e1ae53ec598
Muaz Ahmad <ahmadmua@sheridancollege.ca>
2022-12-14 20:41:18 -0500

    updated

---
+++ API/models/VisualMedia.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.models
+{
+    public interface VisualMedia
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public string Release_date { get; set; }
+        public float Vote_average { get; set; }
+        public string? Poster_path { get; set; }
+        public string Genre { get; set; }
+    }
+}
