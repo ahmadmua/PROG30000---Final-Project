7a4610088a2037de963803d2e6a85bef8ddaa855
Muaz Ahmad <ahmadmua@sheridancollege.ca>
2022-12-17 20:04:43 -0500

    unit test

--- TestingProject/UnitTest1.cs
+++ TestingProject/UnitTest1.cs
@@ -1,10 +1,35 @@
+using Microsoft.AspNetCore.Identity;
+using API.models;
+using API.models.DTOs;
+using API.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+
 namespace TestingProject;
 
 public class UnitTest1
 {
+
+        private SignInManager<AppUser> _signInManager;
+        private UserManager<AppUser> _userManager;
+        
+    
     [Fact]
-    public void Test1()
+    public async Task LoginTestAsync()
     {
+
+                var user = new AppUser() { 
+                
+                    Email = "JohnDoe@gmail.com",
+                    UserName = "JohnDoe",
+                    Review = ""
+                
+                };
+
+             var result = await _userManager.CreateAsync(user, "Abc123me@");
+
+            Assert.True(result.Succeeded);
 
     }
 }
